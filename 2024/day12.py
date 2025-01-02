8711f2a61d42638941c0d420fc0dde3a1cb28691
Giovanni Squillero <squillero@polito.it>
2025-01-02 20:06:44 +0100

    Add day 12

--- 2024/day12.py
+++ 2024/day12.py
@@ -3,24 +3,54 @@
 # SPDX-License-Identifier: 0BSD
 
 from collections import namedtuple
-from itertools import accumulate
+from dataclasses import dataclass
+from itertools import accumulate, product
 import numpy as np
 from icecream import ic
 
 # INPUT_FILE = 'day12-example.txt'
+# INPUT_FILE = 'day12-example_small.txt'
 INPUT_FILE = 'day12-input.txt'
 
+
+@dataclass(frozen=True)
+class Pos:
+    r: int
+    c: int
+
+    def __add__(self, other):
+        return Pos(self.r + other.r, self.c + other.c)
+
+    @property
+    def above(self):
+        return self + Pos(-1, 0)
+
+    @property
+    def below(self):
+        return self + Pos(1, 0)
+
+    @property
+    def right(self):
+        return self + Pos(0, 1)
+
+    @property
+    def left(self):
+        return self + Pos(0, -1)
+
+    @property
+    def neighbors(self):
+        return {self.above, self.right, self.below, self.left}
+
+    @property
+    def i(self):
+        return self.r, self.c
+
+
 SPECIAL = '•'
-Pos = namedtuple('Pos', ['r', 'c'])
-
-
-def get_neighbors(pos):
-    r"""Generates the set of neighbors (N, E, S, W)"""
-    return {Pos(pos.r + r, pos.c + c) for r, c in [(0, -1), (1, 0), (0, +1), (-1, 0)]}
 
 
 def poslist_to_idxs(positions):
-    r"""Transforms a list of `Pos` into `NumPy` index:es"""
+    r"""Transforms a list of `Pos` into `NumPy` indexes"""
     return [p.r for p in positions], [p.c for p in positions]
 
 
@@ -40,29 +70,45 @@ def flood(map_, start):
         queue.extend(
             [
                 p
-                for p in get_neighbors(pos)
-                if map_[p] == map_[start] and p not in area and p not in queue
+                for p in pos.neighbors
+                if map_[p.i] == map_[start.i] and p not in area and p not in queue
             ]
         )
-
-    *_, neighbors = accumulate(area, lambda x, p: x + list(get_neighbors(p) - area), initial=list())
-    map_[poslist_to_idxs(area)] = SPECIAL
-
-    return len(area), len(neighbors)
+    return area
 
 
 def main():
     map_ = np.array([list(line.rstrip()) for line in open(INPUT_FILE)])
     map_ = np.pad(map_, pad_width=1, constant_values=SPECIAL)
 
-    # --- Part One ---
+    # --- Part One and Part Two ---
     price = 0
+    discountedPrice = 0
     while pos := find_region(map_):
-        a, p = flood(map_, pos)
-        price += a * p
-    ic(price)
+        area = flood(map_, pos)
+        *_, borders = accumulate(area, lambda x, p: x + list(p.neighbors - area), initial=list())
+        price += len(area) * len(borders)
 
-    # --- Part Two ---
+        sides = 0
+        for p in area:
+            sides += map_[p.i] != map_[p.above.i] and not (
+                map_[p.left.i] == map_[p.i] and map_[p.left.i] != map_[p.left.above.i]
+            )
+            sides += map_[p.i] != map_[p.below.i] and not (
+                map_[p.left.i] == map_[p.i] and map_[p.left.i] != map_[p.left.below.i]
+            )
+            sides += map_[p.i] != map_[p.right.i] and not (
+                map_[p.above.i] == map_[p.i] and map_[p.above.i] != map_[p.above.right.i]
+            )
+            sides += map_[p.i] != map_[p.left.i] and not (
+                map_[p.above.i] == map_[p.i] and map_[p.above.i] != map_[p.above.left.i]
+            )
+        discountedPrice += sides * len(area)
+
+        map_[poslist_to_idxs(area)] = SPECIAL
+
+    ic(price)
+    ic(discountedPrice)
 
 
 if __name__ == '__main__':
