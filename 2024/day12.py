14d5658a4d5a6b82d24ee15d3570554b5e8725aa
Giovanni Squillero <squillero@polito.it>
2025-01-03 10:54:12 +0100

    Cleanup day 12

--- 2024/day12.py
+++ 2024/day12.py
@@ -13,31 +13,31 @@ INPUT_FILE = 'day12-input.txt'
 
 
 @dataclass(frozen=True)
-class GardenPosition:
+class Position:
     r: int
     c: int
 
     def __add__(self, other):
-        return GardenPosition(self.r + other.r, self.c + other.c)
+        return Position(self.r + other.r, self.c + other.c)
 
-    def __call__(self, map_):
-        return map_[self.r, self.c]
+    def __call__(self, garden):
+        return garden[self.r, self.c]
 
     @property
     def above(self):
-        return self + GardenPosition(-1, 0)
+        return self + Position(-1, 0)
 
     @property
     def below(self):
-        return self + GardenPosition(1, 0)
+        return self + Position(1, 0)
 
     @property
     def right(self):
-        return self + GardenPosition(0, 1)
+        return self + Position(0, 1)
 
     @property
     def left(self):
-        return self + GardenPosition(0, -1)
+        return self + Position(0, -1)
 
     @property
     def neighbors(self):
@@ -48,17 +48,17 @@ SPECIAL = '•'
 
 
 def poslist_to_idxs(positions):
-    r"""Transforms a list of `GardenPosition` into `NumPy` indexes"""
+    r"""Transforms a list of `Position` into `NumPy` indexes"""
     return [p.r for p in positions], [p.c for p in positions]
 
 
-def find_region(map_):
+def find_region(garden):
     r"""Finds a region in the map"""
-    r, c = np.where(map_ != SPECIAL)
-    return GardenPosition(r[0], c[0]) if r.size else None
+    r, c = np.where(garden != SPECIAL)
+    return Position(r[0], c[0]) if r.size else None
 
 
-def flood(map_, start):
+def flood(garden, start):
     r"""Flood fills region starting from `start`, returns area and perimeter"""
     area = set()
     queue = [start]
@@ -69,41 +69,41 @@ def flood(map_, start):
             [
                 p
                 for p in pos.neighbors
-                if p(map_) == start(map_) and p not in area and p not in queue
+                if p(garden) == start(garden) and p not in area and p not in queue
             ]
         )
     return area
 
 
 def main():
-    map_ = np.array([list(line.rstrip()) for line in open(INPUT_FILE)])
-    map_ = np.pad(map_, pad_width=1, constant_values=SPECIAL)
+    garden = np.array([list(line.rstrip()) for line in open(INPUT_FILE)])
+    garden = np.pad(garden, pad_width=1, constant_values=SPECIAL)
 
     # --- Part One and Part Two ---
     price = 0
     discountedPrice = 0
-    while pos := find_region(map_):
-        area = flood(map_, pos)
+    while pos := find_region(garden):
+        area = flood(garden, pos)
         *_, borders = accumulate(area, lambda x, p: x + list(p.neighbors - area), initial=list())
         price += len(area) * len(borders)
 
         sides = 0
         for p in area:
-            sides += p(map_) != p.above(map_) and not (
-                p.left(map_) == p(map_) and p.left(map_) != p.left.above(map_)
+            sides += p(garden) != p.above(garden) and not (
+                p.left(garden) == p(garden) and p.left(garden) != p.left.above(garden)
             )
-            sides += p(map_) != p.below(map_) and not (
-                p.left(map_) == p(map_) and p.left(map_) != p.left.below(map_)
+            sides += p(garden) != p.below(garden) and not (
+                p.left(garden) == p(garden) and p.left(garden) != p.left.below(garden)
             )
-            sides += p(map_) != p.right(map_) and not (
-                p.above(map_) == p(map_) and p.above(map_) != p.above.right(map_)
+            sides += p(garden) != p.right(garden) and not (
+                p.above(garden) == p(garden) and p.above(garden) != p.above.right(garden)
             )
-            sides += p(map_) != p.left(map_) and not (
-                p.above(map_) == p(map_) and p.above(map_) != p.above.left(map_)
+            sides += p(garden) != p.left(garden) and not (
+                p.above(garden) == p(garden) and p.above(garden) != p.above.left(garden)
             )
         discountedPrice += sides * len(area)
 
-        map_[poslist_to_idxs(area)] = SPECIAL
+        garden[poslist_to_idxs(area)] = SPECIAL
 
     ic(price)
     ic(discountedPrice)
