af22de5566b5d726d6abe48112836c39ed24a339
Giovanni Squillero <squillero@polito.it>
2025-01-03 10:52:07 +0100

    Cleanup day 12

--- 2024/day12.py
+++ 2024/day12.py
@@ -2,9 +2,8 @@
 # Copyright 2024 by Giovanni Squillero
 # SPDX-License-Identifier: 0BSD
 
-from collections import namedtuple
 from dataclasses import dataclass
-from itertools import accumulate, product
+from itertools import accumulate
 import numpy as np
 from icecream import ic
 
@@ -14,50 +13,49 @@ INPUT_FILE = 'day12-input.txt'
 
 
 @dataclass(frozen=True)
-class Pos:
+class GardenPosition:
     r: int
     c: int
 
     def __add__(self, other):
-        return Pos(self.r + other.r, self.c + other.c)
+        return GardenPosition(self.r + other.r, self.c + other.c)
+
+    def __call__(self, map_):
+        return map_[self.r, self.c]
 
     @property
     def above(self):
-        return self + Pos(-1, 0)
+        return self + GardenPosition(-1, 0)
 
     @property
     def below(self):
-        return self + Pos(1, 0)
+        return self + GardenPosition(1, 0)
 
     @property
     def right(self):
-        return self + Pos(0, 1)
+        return self + GardenPosition(0, 1)
 
     @property
     def left(self):
-        return self + Pos(0, -1)
+        return self + GardenPosition(0, -1)
 
     @property
     def neighbors(self):
         return {self.above, self.right, self.below, self.left}
-
-    @property
-    def i(self):
-        return self.r, self.c
 
 
 SPECIAL = '•'
 
 
 def poslist_to_idxs(positions):
-    r"""Transforms a list of `Pos` into `NumPy` indexes"""
+    r"""Transforms a list of `GardenPosition` into `NumPy` indexes"""
     return [p.r for p in positions], [p.c for p in positions]
 
 
 def find_region(map_):
     r"""Finds a region in the map"""
     r, c = np.where(map_ != SPECIAL)
-    return Pos(r[0], c[0]) if r.size else None
+    return GardenPosition(r[0], c[0]) if r.size else None
 
 
 def flood(map_, start):
@@ -71,7 +69,7 @@ def flood(map_, start):
             [
                 p
                 for p in pos.neighbors
-                if map_[p.i] == map_[start.i] and p not in area and p not in queue
+                if p(map_) == start(map_) and p not in area and p not in queue
             ]
         )
     return area
@@ -91,17 +89,17 @@ def main():
 
         sides = 0
         for p in area:
-            sides += map_[p.i] != map_[p.above.i] and not (
-                map_[p.left.i] == map_[p.i] and map_[p.left.i] != map_[p.left.above.i]
+            sides += p(map_) != p.above(map_) and not (
+                p.left(map_) == p(map_) and p.left(map_) != p.left.above(map_)
             )
-            sides += map_[p.i] != map_[p.below.i] and not (
-                map_[p.left.i] == map_[p.i] and map_[p.left.i] != map_[p.left.below.i]
+            sides += p(map_) != p.below(map_) and not (
+                p.left(map_) == p(map_) and p.left(map_) != p.left.below(map_)
             )
-            sides += map_[p.i] != map_[p.right.i] and not (
-                map_[p.above.i] == map_[p.i] and map_[p.above.i] != map_[p.above.right.i]
+            sides += p(map_) != p.right(map_) and not (
+                p.above(map_) == p(map_) and p.above(map_) != p.above.right(map_)
             )
-            sides += map_[p.i] != map_[p.left.i] and not (
-                map_[p.above.i] == map_[p.i] and map_[p.above.i] != map_[p.above.left.i]
+            sides += p(map_) != p.left(map_) and not (
+                p.above(map_) == p(map_) and p.above(map_) != p.above.left(map_)
             )
         discountedPrice += sides * len(area)
 
