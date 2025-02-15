7548d5f7333edb9519b91e9c8151dcb48e0c628c
Giovanni Squillero <squillero@polito.it>
2025-02-15 19:25:34 +0100

    Add 2nd half of day 14

--- 2024/day14.py
+++ 2024/day14.py
@@ -7,10 +7,11 @@ from operator import mul
 from functools import reduce
 import re
 from icecream import ic
-from tqdm.auto import tqdm
+import numpy as np
 
 # INPUT_FILE = 'day14-example.txt'
 INPUT_FILE = 'day14-input.txt'
+# INPUT_FILE = 'day14-debug.txt'
 
 SPACE_WIDTH = 101
 SPACE_HEIGHT = 103
@@ -27,19 +28,20 @@ def read_robots(filename):
 def robot_step(robot):
     r"""Perform 1 step fo the robot"""
     return Robot(
-        x=(robot.x + robot.vx) % SPACE_WIDTH,
-        y=(robot.y + robot.vy) % SPACE_HEIGHT,
+        x=(robot.x + SPACE_WIDTH + robot.vx) % SPACE_WIDTH,
+        y=(robot.y + SPACE_HEIGHT + robot.vy) % SPACE_HEIGHT,
         vx=robot.vx,
         vy=robot.vy,
     )
 
 
 def main():
-    robots = read_robots(INPUT_FILE)
+    robots_init = read_robots(INPUT_FILE)
 
     # --- Part One ---
-    for _ in tqdm(range(100)):
-        robots = map(robot_step, robots)
+    robots = list(robots_init)
+    # for _ in tqdm(range(100)):
+    #    robots = map(robot_step, robots)
 
     q = [0, 0, 0, 0]
     for r in robots:
@@ -56,6 +58,30 @@ def main():
     ic(safety_factor)
 
     # --- Part Two ---
+    # PIXEL_SIZE = 5
+    # pygame.init()
+    # window = pygame.display.set_mode((SPACE_WIDTH * PIXEL_SIZE, SPACE_HEIGHT * PIXEL_SIZE))
+    # pygame.display.set_caption('AOC Day 14')
+    # font = pygame.font.Font('freesansbold.ttf', 20)
+    robots = list(robots_init)
+
+    steps = 0
+    easter_egg = False
+    while not easter_egg:
+        m = np.zeros((SPACE_HEIGHT, SPACE_WIDTH))
+        for r in robots:
+            m[SPACE_HEIGHT - 1 - r.y, r.x] = 1
+        if max(m.sum(axis=0)) > 20 and max(m.sum(axis=1)) > 20:
+            ic(steps)
+            for r in range(SPACE_HEIGHT):
+                for c in range(SPACE_WIDTH):
+                    print('#' if m[SPACE_HEIGHT - 1 - r, c] == 1 else ' ', end='')
+                print()
+            print()
+            exit()
+
+        steps += 1
+        robots = list(map(robot_step, robots))
 
 
 if __name__ == '__main__':
