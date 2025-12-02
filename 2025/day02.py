3fa166671391f5e766977ea6549f51b85255a739
Giovanni Squillero <giovanni.squillero@polito.it>
2025-12-02 18:33:31 +0100

    Add alternative solution to Day2

--- 2025/day02.py
+++ 2025/day02.py
@@ -3,58 +3,89 @@
 # SPDX-License-Identifier: 0BSD
 
 from itertools import product
+import re
 from icecream import ic
 
 # INPUT_FILE_NAME = 'day02-test.txt'
 INPUT_FILE_NAME = 'day02-input.txt'
 
 
-def invalid_ids(num_digits):
-    if num_digits % 2 == 1:
-        return
-    for x in product('0123456789', repeat=num_digits // 2):
-        if x[0] == '0':
-            continue  # Yeuch
-        yield ''.join(x) * 2
+def create_id(symbols, length) -> set[str]:
+    return set(''.join(d) for d in product(symbols, repeat=length))
 
 
-def invalid_ids_v2(num_digits):
+def invalid_ids_p1(num_symbols) -> set[str]:
+    if num_symbols == 0:
+        return set()
+    else:
+        return set(
+            p1 + p2
+            for p1, p2 in product(
+                create_id('123456789', 1), create_id('0123456789', num_symbols - 1)
+            )
+        )
+
+
+def invalid_ids_p2(num_digits):
     invalid = set()
-    for c0 in '123456789':
-        for cn in (
-            ''.join(c) for s in range(num_digits // 2) for c in product('0123456789', repeat=s)
-        ):
-            id_ = (c0 + cn) * (num_digits // len(c0 + cn))
-            if len(id_) == num_digits:
-                invalid.add(id_)
+    for n in range(1, num_digits):
+        if num_digits % n == 0:
+            invalid |= {i * (num_digits // n) for i in invalid_ids_p1(n)}
     return invalid
 
 
+# First idea: create all illegal ids and check if they are in range
+def generate_all_illegal_ids(id_ranges):
+    # Part 1
+    tot_invalid = 0
+    for from_, to_ in id_ranges:
+        for id_ in set.union(
+            *[invalid_ids_p1(d) for d in range(len(from_) // 2, len(to_) // 2 + 1)]
+        ):
+            id_ *= 2
+            if int(from_) <= int(id_) <= int(to_):
+                tot_invalid += int(id_)
+    ic(tot_invalid)
+
+    # Part 2
+    tot_invalid = 0
+    for from_, to_ in id_ranges:
+        for id_ in set.union(*[invalid_ids_p2(d) for d in range(len(from_), len(to_) + 1)]):
+            if int(from_) <= int(id_) <= int(to_):
+                tot_invalid += int(id_)
+    ic(tot_invalid)
+
+
+# Second idea: create a check for illegal ones using regex
+def generate_all_ids(id_ranges):
+    pattern = re.compile(r'^(.+)\1$')
+    tot_invalid = 0
+    for from_, to_ in id_ranges:
+        for n in range(int(from_), int(to_) + 1):
+            if pattern.match(str(n)):
+                tot_invalid += n
+    ic(tot_invalid)
+
+    pattern = re.compile(r'^(.+)\1+$')
+    tot_invalid = 0
+    for from_, to_ in id_ranges:
+        for n in range(int(from_), int(to_) + 1):
+            if pattern.match(str(n)):
+                tot_invalid += n
+    ic(tot_invalid)
+
+
 def main():
+    ranges = list()
     with open(INPUT_FILE_NAME) as file:
-        id_ranges = file.read().split(',')
+        for r in file.read().split(','):
+            ranges.append(tuple(r.split('-')))
 
-    # v1
-    tot_invalid = 0
-    for id_range in id_ranges:
-        from_, to_ = id_range.split('-')
-        for digits in range(len(from_), len(to_) + 1):
-            for id_ in invalid_ids(digits):
-                if int(from_) <= int(id_) <= int(to_):
-                    tot_invalid += int(id_)
-                    # ic(id_)
-    ic(tot_invalid)
+    # v1 - faster ;-)
+    generate_all_illegal_ids(ranges)
 
-    # v2
-    tot_invalid = 0
-    for id_range in id_ranges:
-        from_, to_ = id_range.split('-')
-        for digits in range(len(from_), len(to_) + 1):
-            for id_ in invalid_ids_v2(digits):
-                if int(from_) <= int(id_) <= int(to_):
-                    tot_invalid += int(id_)
-                    # ic(id_)
-    ic(tot_invalid)
+    # v2 - slower :-(
+    generate_all_ids(ranges)
 
 
 if __name__ == '__main__':
