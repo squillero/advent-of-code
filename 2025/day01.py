e0cf2b096436b32c66d385c7631078e4ed8ed3a6
Giovanni Squillero <giovanni.squillero@polito.it>
2025-12-02 10:25:24 +0100

    Start 2025!

---
+++ 2025/day01.py
@@ -0,0 +1,47 @@
+# Advent of Code 2025 | https://adventofcode.com/2025/day/1
+# Copyright 2025 by Giovanni Squillero
+# SPDX-License-Identifier: 0BSD
+
+from icecream import ic
+
+INPUT_FILE_NAME = 'day01-test.txt'
+INPUT_FILE_NAME = 'day01-input.txt'
+
+
+def part_one(file_name):
+    dial = 50
+    password = 0
+    with open(INPUT_FILE_NAME) as file:
+        for line in file:
+            d, offset = line[0], int(line[1:])
+            dial += offset if d == 'R' else -offset
+            if not dial % 100:
+                password += 1
+    return password
+
+
+def part_two(file_name):
+    dial = 50
+    password = 0
+    with open(INPUT_FILE_NAME) as file:
+        for line in file:
+            if line[0] == 'R':
+                step = +1
+            else:
+                step = -1
+            for _ in range(abs(int(line[1:]))):
+                dial += step
+                if not dial % 100:
+                    password += 1
+    return password
+
+
+def main():
+    password = part_one(INPUT_FILE_NAME)
+    ic(password)
+    password = part_two(INPUT_FILE_NAME)
+    ic(password)
+
+
+if __name__ == '__main__':
+    main()
