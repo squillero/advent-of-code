26e9027f0a62ac1453d1600ed90c5c00ceac5458
Giovanni Squillero <giovanni.squillero@polito.it>
2025-12-13 12:03:03 +0100

    Add day 5

--- 2025/day05.py
+++ 2025/day05.py
@@ -7,7 +7,7 @@ from itertools import combinations
 from icecream import ic
 
 INPUT_FILE_NAME = 'day05-test.txt'
-# INPUT_FILE_NAME = 'day05-input.txt'
+INPUT_FILE_NAME = 'day05-input.txt'
 
 Range = namedtuple('Range', ['s', 'e'])
 
@@ -16,28 +16,32 @@ def main():
     with open(INPUT_FILE_NAME) as file:
         ranges_raw, ingredient_raw = file.read().split('\n\n')
 
-    # An excercise on list comprehension & generators
+    # Part1: An excercise on list comprehension & generators
     ranges = {Range(*map(int, line.split('-'))) for line in ranges_raw.split()}
     ingredients = [int(i) for i in ingredient_raw.split()]
     fresh_count = sum(any(rs <= i <= re for rs, re in ranges) for i in ingredients)
     ic(fresh_count)
 
+    # Part2: Join & prune ranges
     stable = False
     while not stable:
-        new_ranges = set()
         for s1, s2 in combinations(sorted(ranges), r=2):
             if s1.s <= s2.s <= s1.e <= s2.e:
-                new_ranges.add(Range(s1.s, s2.e))
+                # ie. <1 <2 1> 2>
+                ranges.remove(s1)
+                ranges.remove(s2)
+                ranges.add(Range(s1.s, s2.e))
+                break
             elif s1.s <= s2.s <= s2.e <= s1.e:
-                new_ranges.add(Range(s1.s, s1.e))
-            elif s2.s <= s1.s <= s1.e <= s2.e:
-                pass
-            else:
-                new_ranges.add(s1)
-                new_ranges.add(s2)
-        stable = new_ranges == ranges
-        ic(sorted(new_ranges))
-    ic(sorted(ranges))
+                # ie. <1 <2 2> 1>
+                ranges.remove(s2)
+                break
+        else:
+            stable = True
+    num_fresh = 0
+    for s, e in ranges:
+        num_fresh += e - s + 1
+    ic(num_fresh)
 
 
 if __name__ == '__main__':
